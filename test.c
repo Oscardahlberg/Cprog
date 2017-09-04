790faeba1eef288062a42015486258871213bb42
Oscardahlberg <31594957+Oscardahlberg@users.noreply.github.com>
2017-09-04 10:29:46 +0200

    Improved program

--- test.c
+++ test.c
@@ -1,5 +1,23 @@
-#include <stdoi.h>
+#include <stdio.h>
+int main ()
+{
+   int c_h, c_m, c_s;
+   int a_h, a_m, a_s;
 
-int main(){
-	return 0;
+   printf("What is current time?\n");
+   scanf("%d:%d:%d", &c_h, c_m, c_s);
+   printf("What is time of alarm?\n");
+   scanf("%d:%d:%D", &a_h, a_m, a_s);
+
+
+   while (current_time < alarm_time)
+   {
+
+      printf("%d\n", current_time);
+      current_time = current_time + 1;
+   }
+
+printf("Alarm\n");
+
+   return 0;
 }
