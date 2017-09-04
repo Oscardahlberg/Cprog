16bbf2ebfe6e4d1308dbe031cd9d94246e98eff4
Oscardahlberg <31594957+Oscardahlberg@users.noreply.github.com>
2017-09-04 16:16:21 +0200

    Finishd

--- test.c
+++ test.c
@@ -1,23 +1,38 @@
 #include <stdio.h>
+
 int main ()
 {
-   int c_h, c_m, c_s;
-   int a_h, a_m, a_s;
+   int current_time;
+   int alarm_time;
+   int s = 1;
 
    printf("What is current time?\n");
-   scanf("%d:%d:%d", &c_h, c_m, c_s);
+   scanf("%d", &current_time);
    printf("What is time of alarm?\n");
-   scanf("%d:%d:%D", &a_h, a_m, a_s);
+   scanf("%d", &alarm_time);
 
 
-   while (current_time < alarm_time)
+   while (s)
    {
 
-      printf("%d\n", current_time);
+      if (current_time == 240000)
+      {
+         current_time = 0;
+      }
+
+      if (current_time == alarm_time){
+         s = 0;
+      }
+      else
+      {
+         printf("%d\n", current_time);
+      }
+
       current_time = current_time + 1;
    }
 
-printf("Alarm\n");
+
+   printf("Alarm\n");
 
    return 0;
 }
